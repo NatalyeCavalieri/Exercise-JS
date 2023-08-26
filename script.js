81f7898cbd2138af61ca801518da7c62be9928c6
Natalye Cavalieri <nahcavalieri@gmail.com>
2023-08-26 12:42:24 -0300

    Exercise JS

---
+++ script.js
@@ -0,0 +1,110 @@
+/*Você pode apresentar o resultado tanto no **console** quanto em um **alerta** no navegador.*/
+
+//1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+//alert ("Hello World")
+
+//2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+/*
+let firstNumber= 6
+let secondNumber= 4
+
+let sumNumber = (firstNumber + secondNumber)
+alert (sumNumber) */
+
+/*3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, 
+exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+    💡 Para saber o tipo de dado você pode usar o operador `typeof` */
+
+/*
+let whatType = "40028922"
+
+if (typeof whatType === 'number'){
+alert("É um número")
+} else {
+  alert("Não é um número")
+}
+*/
+
+/*4. Crie um script que declare uma variável e verifique se o seu valor é uma string. 
+Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".*/
+
+/*
+let whatType = "Natalye"
+
+if(typeof whatType === "string"){
+  alert("é uma string")
+}else{
+  alert("Nao é uma string")
+}
+*/
+
+/*5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. 
+Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".*/
+
+/*
+
+let whatType = false
+
+if(typeof whatType === `boolean`){
+  alert("É um booleano")
+}else{
+  alert("Não é um booleano")
+}
+
+*/
+
+/*6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+let firstNumber= 10
+let secondNumber= 3
+
+let sumNumber= (firstNumber - secondNumber)
+alert(sumNumber)
+
+
+*/
+
+/*Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+let firstNumber = 5
+let secondNumber = 3
+
+let multNumber = (firstNumber * secondNumber)
+alert(multNumber)
+*/
+
+/*7. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+let firstNumber= 20
+let secondNumber=4
+
+let divNumber = (firstNumber / secondNumber)
+alert(divNumber)
+*/
+
+/*Crie um script que declare uma variável e verifique se o seu valor é um número par. 
+Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+//obs: variável % 2 === 0 (numero par) != de 0 (numero impar)
+
+let number = 4
+
+if(number % 2 === 0){
+  alert("O número é par")
+}else{
+  alert("O número é impar")
+}
+
+*/
+
+/*Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. 
+Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+let number = 7
+
+if(number % 2 !== 0){
+  alert("É um número impar")
+}else{
+  alert("É um número par")
+}
+
+*/
